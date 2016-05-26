0224e8bd8ad9f1950e07a159d1683a8d8f736afe
Dkuran <kamelotd13@hotmail.com>
2016-05-26 10:30:30 -0500

    directiva ng-repeat

--- Gemas/js/gemaCtrl.js
+++ Gemas/js/gemaCtrl.js
@@ -2,13 +2,48 @@ app.controller('gemaController', function(){
 
 console.log('mi primer controlador');
 
-  var gem = {
+//  var gem = {
+//
+//    nombre: 'gema1',
+//    precio: 2.95,
+//    descripcion: 'gema de corte refinado',
+//    compra: true
+//  }
 
-    nombre: 'gema1',
-    precio: 2.95,
-    descripcion: 'gema de corte refinado',
-    compra: true
-  }
+  var gem = [
+
+    {
+
+      nombre: 'gema1',
+      precio: 2.95,
+      descripcion: 'gema de corte refinado',
+      compra: true
+    },
+    {
+
+      nombre: 'gema2',
+      precio: 3.95,
+      descripcion: 'gema de corte refinado',
+      compra: false
+    },
+    {
+
+      nombre: 'gema3',
+      precio: 4.95,
+      descripcion: 'gema de corte refinado',
+      compra: true
+    },
+    {
+
+      nombre: 'gema4',
+      precio: 5.95,
+      descripcion: 'gema de corte refinado',
+      compra: false
+    }
+
+
+
+  ]
 
 
   this.product = gem;
