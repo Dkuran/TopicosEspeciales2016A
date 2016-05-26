741c69f0b4cdc8aeb40601f172fefbd57995339a
Dkuran <kamelotd13@hotmail.com>
2016-05-26 09:29:46 -0500

    uso del primer controlador

---
+++ Gemas/js/gemaCtrl.js
@@ -0,0 +1,16 @@
+app.controller('gemaController', function(){
+
+console.log('mi primer controlador');
+
+  var gem = {
+
+    nombre: 'gema1',
+    precio: 2.95,
+    descripcion: 'gema de corte refinado',
+    compra: true
+  }
+
+
+  this.product = gem;
+
+});
